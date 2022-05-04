463e07c2466ab8983ca603181c09a45a083b1d53
sakajamiu <sakaj20@gmail.com>
2022-05-04 17:25:49 +0100

    'end-of-part1'

---
+++ apollo-client.ts
@@ -0,0 +1,12 @@
+import { ApolloClient,createHttpLink,InMemoryCache } from '@apollo/client'
+
+const httpLink = createHttpLink({
+    uri:'http://localhost/4000'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+export default client
