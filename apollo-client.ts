71dced4a0c26125571cb98e4ec0872615e8eb1ab
sakajamiu <sakaj20@gmail.com>
2022-05-05 23:14:24 +0100

    'implementing-subscription'

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,6 +1,9 @@
 import { ApolloClient,createHttpLink,InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context';
 import { AUTH_TOKEN } from './constraint';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const httpLink = createHttpLink({
     uri:'http://localhost:4000'
@@ -13,9 +16,29 @@ const authLink = setContext((_, { headers }) => {
         authorization: token ? `Bearer ${token}` : ''
       }
     };
-  });
+ });
+ const wsLink :any= typeof window !=='undefined'? new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+      reconnect: true,
+      connectionParams: {
+        authToken: localStorage.getItem(AUTH_TOKEN)
+      }
+    }
+  }): null;
+  const link = typeof window !=='undefined'?split(
+    ({ query }) => {
+      const { kind, operation }:any = getMainDefinition(query);
+      return (
+        kind === 'OperationDefinition' &&
+        operation === 'subscription'
+      );
+    },
+    wsLink,
+    authLink.concat(httpLink)
+  ):httpLink;
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 })
 
--- components/LinkList/LinkList.tsx
+++ components/LinkList/LinkList.tsx
@@ -25,9 +25,57 @@ export const FEED_QUERY = gql`
     }
       
   }
-`
+`;
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+const NEW_LINKS_SUBSCRIPTION = gql`
+subscription {
+  newLink {
+    id
+    url
+    description
+    createdAt
+    postedBy {
+      id
+      name
+    }
+    votes {
+      id
+      user {
+        id
+      }
+    }
+  }
+}
+`;
 interface Feed {
+    newLink: any;
     feed:{
+        __typename: any,
         id : String;
         links: Array<{
             id: String | any;
@@ -49,7 +97,33 @@ interface Feed {
 }
 
 export const LinkList = () => {
-    const   {loading, data}= useQuery<Feed>(FEED_QUERY)
+    const{ 
+         data,
+        loading,
+        error,
+        subscribeToMore }= useQuery<Feed>(FEED_QUERY)
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, { subscriptionData }) => {
+              if (!subscriptionData.data) return prev;
+              const newLink = subscriptionData.data.newLink;
+              const exists = prev.feed.links.find(
+                ({ id }) => id === newLink.id
+              );
+              if (exists) return prev;
+          
+              return Object.assign({}, prev, {
+                feed: {
+                  links: [newLink, ...prev.feed.links],
+                  count: prev.feed.links.length + 1,
+                  __typename: prev.feed.__typename
+                }
+              });
+            }
+          });
+          subscribeToMore({
+            document: NEW_VOTES_SUBSCRIPTION
+          });
     if(loading){
         return <div>loading...</div>
     }
